fc0ccb0e5c58a843675812c02ca4a2402f296d46
John Kraus <john@johnfkraus.com>
2022-02-03 11:54:00 -0500

    added push image to ecr in ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -4,10 +4,14 @@ variable "ecr-repo-name" {
 }
 
 resource "aws_ecr_repository" "advanced-ecr-repo" {
-  name = var.ecr-repo-name # "advanced-ecr-repo"
+  name                 = var.ecr-repo-name # "advanced-ecr-repo"
+  image_tag_mutability = "IMMUTABLE"
 
   provisioner "local-exec" {
-    # command = "echo ${self.private_ip} >> private_ips.txt"
+    command = "aws ecr get-login-password --region ${var.region} --profile ${var.aws_profile} | docker login --username AWS --password-stdin 506504484053.dkr.ecr.us-east-1.amazonaws.com"
+  }
+
+  provisioner "local-exec" {
     command = "docker push 506504484053.dkr.ecr.us-east-1.amazonaws.com/${var.ecr-repo-name}:latest"
   }
 }
